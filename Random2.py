f72d03d2129cad5f6b8c87cdfcf449b634da99dd
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-04-04 19:46:22 +0530

    Add files via upload

---
+++ Random2.py
@@ -0,0 +1,8 @@
+import numpy as np # import numpy package
+import random # import random module
+rd_no = np.random.randint(1,4)
+rd_2D_arr = np.random.randint(1,4, (4,4))
+rd_3D_arr = np.random.randint(1,4, (2,4,4)) 
+print(rd_no)
+print(rd_2D_arr)
+print(rd_3D_arr)
