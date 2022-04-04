5bfe77233062d1c7217a3970eb1e360893a1996e
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-04-04 19:13:24 +0530

    Add files via upload

---
+++ Tangent.py
@@ -0,0 +1,13 @@
+import numpy as np # import numpy package
+sin_90 = np.sin(90) # sine value of degree 90 in degree
+print("Sine value of angle 90 in degree = ",sin_90) # print sine value
+sin_90 = np.sin(90 * np.pi/180) # sine value of angle 90 in radinas
+print("Sine value of angle 90 in radians = ", sin_90)
+cos_180 = np.cos(180) # cosine value of degree 180 in degree
+print("Cosine value of angle 180 in degree = ", cos_180) # print cosine value
+cos_180 = np.cos(180 * np.pi/180)
+print("Cosine value of angle 180 in radians = ", cos_180)
+tan_60 = np.tan(60) # tangent value of degree 60 in degree
+print("Tangent value of angle 60 in degree = ", tan_60) # print tangent value
+tan_60 = np.tan(60 * np.pi/180) # tangent value of degree 60 in degree
+print("Tangent value of angle 60 in radians = ", tan_60) # print tangent value
