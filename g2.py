3892367e0cc649b92eaebee72f2d1eedac1fb46a
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-04-04 19:43:03 +0530

    Add files via upload

---
+++ g2.py
@@ -0,0 +1,9 @@
+import numpy as np # import numpy package
+import matplotlib.pyplot as plt # import matplotlib.pyplot package
+x = np.arange(0, 3 * np.pi, 0.1) # create x array of angels from range 0 to 3*3.14
+y = np.cos(x) # create y array of sine values of angles from x array
+plt.plot(x, y) # plot grah 
+plt.title(" Graphical Representation of cosine function")
+plt.xlabel("x axis ")
+plt.ylabel("y axis ")
+plt.show() # show plotted graph
